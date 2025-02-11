b75a38cc558a4c8c0d7e71323929cfff2c5f0b8b
lexardi <lexardi.lab@gmail.com>
2025-02-11 08:24:10 +0100

    cloudinary

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,6 +1,19 @@
+"use client";
 
+import { CldImage } from "next-cloudinary";
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <CldImage
+        src="https://res.cloudinary.com/dxkxg2ivb/image/upload/v1739097872/harri.cc/harri_1_kshoj7.jpg" // Use this sample image or upload your own via the Media Explorer
+        width="700" // Transform the image: auto-crop to square aspect_ratio
+        height="700"
+        alt="Harri.cc"
+        crop={{
+          type: "auto",
+          source: true,
+        }}
+      />
+    </div>
   );
 }
