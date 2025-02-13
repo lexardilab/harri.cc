8b0002ed41e4658fd99fed00eb06abad9f85f2c6
lexardi <lexardi.lab@gmail.com>
2025-02-13 11:23:55 +0100

    totes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,7 +6,7 @@ export default function Content() {
   return (
     <>
       <main className="grid lg:grid-cols-2 gap-1 sm:grid-cols-1 bg-[#f3f3f5]">
-        <div className="lg:pl-12 lg:pt-12 px-2  items-center flex justify-center">  
+        <div className="flex items-center justify-center">  
         <CldImage
             src="https://res.cloudinary.com/dxkxg2ivb/image/upload/v1739441637/harri.cc/Camiseta%20Azpitik/Tote_Azpitik_Mustard_xkmxp9.webp" // Use this sample image or upload your own via the Media Explorer
             width="700" // Transform the image: auto-crop to square aspect_ratio
@@ -18,7 +18,7 @@ export default function Content() {
             }}
           />
         </div>
-        <div className="flex lg:justify-end lg:pr-12 px:6">
+        <div className="flex items-center justify-center">
           <CldImage
             src="https://res.cloudinary.com/dxkxg2ivb/image/upload/v1739350565/harri.cc/Camiseta%20Azpitik/Tote_Azpitik_OrangeRust_kryszv.webp" // Use this sample image or upload your own via the Media Explorer
             width="700" // Transform the image: auto-crop to square aspect_ratio
